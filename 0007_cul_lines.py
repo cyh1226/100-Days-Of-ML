f3a6f0da6bffa5767d1772d15691989555ac8497
cyh1226 <yuhanchen1226@gmail.com>
2018-09-20 19:04:52 +0800

    code num calculate
    文件打开时需注意打开的文件编码，此处为utf-8

--- 0007_cul_lines.py
+++ 0007_cul_lines.py
@@ -1,1 +1,37 @@
-import sys
+import os
+import time
+
+def filelist(orginpath):
+	for parent,dirnames,filenames in os.walk(orginpath):
+		fl = list()
+		for filename in filenames:
+			if filename.split('.')[-1] == 'py':
+				fl.append(os.path.join(parent,filename))
+	return fl
+
+def count(files):
+	blank_num = 0
+	anno_num = 0
+	code_num = 0
+	t_blank_num = 0
+	t_anno_num = 0
+	t_code_num = 0
+	for f in files:
+		for line in open(f, encoding = 'utf-8').readlines():
+			line = line.strip()
+			if line == '':
+				blank_num += 1
+			elif line.startswith('#'):
+				anno_num += 1
+			else:
+				code_num += 1
+		print(f + '\t' + str(blank_num) + '\t' + str(anno_num) + '\t' + str(code_num))
+		t_blank_num += blank_num
+		t_anno_num += anno_num
+		t_code_num += code_num
+	print('Total num are listed' + '\t' + str(t_blank_num) + '\t' + str(t_anno_num) + '\t' + str(t_code_num))
+
+
+if __name__ == '__main__':
+	filepath = r'E:\python-程序'
+	count(filelist(filepath))
